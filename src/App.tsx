d164e2fa1660cb365f2ba7735adc7dadf0516c36
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-22 00:32:13 +0300

    fix:[MASTER][Vadim]: added packages for translatable

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,26 @@
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-	const {
-		t,
-		i18n: { changeLanguage, language }
-	} = useTranslation()
-	const [currentLanguage, setCurrentLanguage] = useState<string>(language)
-
-	const handleChangeLanguage = () => {
-		const newLanguage = currentLanguage === 'en' ? 'tr' : 'en'
-		setCurrentLanguage(newLanguage)
-		changeLanguage(newLanguage)
-	}
-
 	return (
 		<>
 			<div>
-				<a href='https://vite.dev' target='_blank'>
+				<a href='#'>
 					<img src={viteLogo} className='logo' alt='Vite logo' />
 				</a>
-				<a href='https://react.dev' target='_blank'>
+				<a href='#'>
 					<img src={reactLogo} className='logo react' alt='React logo' />
 				</a>
 			</div>
 			<h1>Vite + React</h1>
 			<div className='card'>
-				<button onClick={handleChangeLanguage}>Language is {language}</button>
+				<button>Change Language</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className='read-the-docs'>{t(`react`)}</p>
+			<p className='read-the-docs'>{'Language is:'}</p>
 		</>
 	)
 }
